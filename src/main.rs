60617e2741bbe4ce923c8627a57f05e2f0329a3a
Clayton Andrade <clayton.andrade2009@gmail.com>
2020-09-21 17:26:09 -0300

    Segundo Commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let mut v = vec![1, 2, 3, 4, 5];
     println!("{:?}", v);
@@ -45,4 +47,58 @@ fn main() {
     for b in s.bytes() {
         println!("{}", b);
     }
+
+    let mut placares = HashMap::<String, u32>::new();
+    // let mut placares: HashMap<String, u32> = HashMap::new();
+    placares.insert("Azul".to_string(), 10);
+    // placares.insert("Amarelo".to_string(), 50);
+    placares.entry("Amarelo".to_string()).or_insert(50);
+    placares.entry("Azul".to_string()).or_insert(50);
+    println!("{:?}", placares);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+    let scores: HashMap<_, _> = teams.into_iter().zip(initial_scores.into_iter()).collect();
+    println!("{:?}", scores);
+
+    let name = "Color".to_string();
+    let value = "Blue".to_string();
+
+    let mut map = HashMap::new();
+    map.insert(name, value);
+
+    println!("{:?}", map);
+    // println!("{}, {}", name, value);
+
+    let team_name = "Blue".to_string();
+    let score = scores.get(&team_name);
+    match score {
+        Some(x) => println!("Score: {}", x),
+        None => println!("Not found.")
+    }
+
+    for (k, v) in &placares {
+        println!("{}: {}", k, v);
+    }
+
+    let words = "hello world wonderful world";
+    let mut map = HashMap::new();
+
+    for word in words.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
+
+    let letters = "lakahstshfnfksmsmsmsjjjystsgdrndsççççsksjsustegd".to_string();
+    let mut map_letters = HashMap::new();
+
+    let mut count;
+    for l in letters.chars() {
+        count = map_letters.entry(l).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map_letters);
 }
